044d3415400166dd36b15a39198e98e76f9ade33
Rasel Hossain Adib <97701792+AdibRasel@users.noreply.github.com>
2025-01-13 21:56:34 +0600

    dart class static

--- Dart_Object-Oriented_Programming/ExternalClass.dart
+++ Dart_Object-Oriented_Programming/ExternalClass.dart
@@ -2,7 +2,18 @@
 // এবং একটা ক্লাসের ভিতরে আরো অনেক গুলো ক্লাস ডিক্লেয়ার করা যায়।
 // প্রয়োজন অনুযায়ি ক্লাস থেকে সকল প্রোপার্টিজ এবং মেথড এক্সেস করা যায়।
 class MyClass {
-  // Properties (প্রোপার্টিজ)
+
+  static var Name = "Rasel Hossain Adib";
+  static var SchoolName = "Shahapur M.L High School";
+  static List MobileNumber = [01934544352, 01626757897];
+  static Map Address = {
+    'Home': 'Shibrampur, Shahapur, Chatkhil, Noakhali',
+    'Office': 'Dhaka, Bangladesh'
+  };
+  static Function SubmitButton = () {
+    print('Submit Button Clicked');
+  };
+
   var name = "Rasel Hossain Adib";
   List mobileNumber = [01934544352, 01626757897];
   Map address = {
@@ -11,7 +22,7 @@ class MyClass {
   };
 
   // Method (মেথড)
-  void submitButton() {
+  static void submitButton() {
     print('Submit Button Clicked');
   }
 }
--- Dart_Object-Oriented_Programming/OOPClass.dart
+++ Dart_Object-Oriented_Programming/OOPClass.dart
@@ -2,18 +2,14 @@
 import 'ExternalClass.dart';
 
 void main() {
-  // Object creation (অবজেক্ট তৈরি) এক্সটার্নাল ক্লাস হলে আগে ইম্পোর্ট করতে হবে।
-  var classCall = new MyClass(); // new কি-ওয়ার্ড ব্যবহার করা হয়েছে কিন্তু এটা ব্যবহার না করা হলে কোন সমস্যা নেই। new কি-ওয়ার্ড ব্যবহার করা বেস্ট প্র্যাকটিস।
+  // var classCall = new MyClass(); // static ব্যবহার করলে অভজেক্ট তৈরি করতে হয় না।
 
-  // Accessing properties and methods (প্রোপার্টিজ এবং মেথড অ্যাক্সেস করা)
-  var myName = classCall.name; // ক্লাসের অবজেক্ট থেকে ভ্যারিয়েবল এক্সেস করা
-  var myMobileNumber = classCall.mobileNumber; // ক্লাসের অবজেক্ট থেকে লিস্ট এক্সেস করা
-  var myAddress = classCall.address; // ক্লাসের অবজেক্ট থেকে ম্যাপ এক্সেস করা
+  // static ব্যবহার কররতে হলে প্রথমে ইম্পোর্ট করতে হবে। 
+  // তারপর ক্লাসের নাম দিয়ে ডট নোটেশন দিয়ে প্রোপার্টিজ এবং মেথড এক্সেস করতে হবে।
+  var Name = MyClass.Name;
+  var SchoolName = MyClass.SchoolName;
+  var Address = MyClass.Address;
+  var MobileNumber = MyClass.MobileNumber;
+  var SubmitButton = MyClass.SubmitButton;
 
-  // সকল তথ্য প্রিন্ট করা
-  print('Name: $myName, My Number: $myMobileNumber, My Address: $myAddress');
-  // রেজাল্ট: Name: Rasel Hossain Adib, My Number: [1934544352, 1626757897], My Address: {Home: Shibrampur, Shahapur, Chatkhil, Noakhali, Office: Dhaka, Bangladesh}
-  
-  classCall.submitButton(); // ক্লাসের অবজেক্ট থেকে মেথড এক্সেস করা
-  // রেজাল্ট: Submit Button Clicked
 }
